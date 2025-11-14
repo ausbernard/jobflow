6a6a0036c0ca67bdf262af66b170479c8085065d
ausbernard <109692469+ausbernard@users.noreply.github.com>
2025-11-14 09:23:33 -0500

    bug: remove pytest lines in pyproject & linter

--- src/app/models.py
+++ src/app/models.py
@@ -1,11 +1,13 @@
-from pydandic import BaseModel, Field                   #library that gives you data validation and type-safe models.
-from typing import Literal, Optional, Dict, Any         #
+from pydandic import BaseModel, Field
+from typing import Literal, Optional, Dict, Any
 
 JobType = Literal["shell", "python"]
+
 
 class JobCreate(BaseModel):
     type: JobType
     payload: Dict[str, Any] = Field(default_factory=dict)
+
 
 class Job(BaseModel):
     id: str
